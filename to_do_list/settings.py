d4a623aca81d13d7fa3170925da67b85a917147c
Robson J <robsonjrservices@gmail.com>
2023-11-24 21:26:38 -0300

    FEAT: Pre-structure done with 'list' application done

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'list',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILERS_URL = [os.path.join(BASE_DIR, STATIC_URL)]
